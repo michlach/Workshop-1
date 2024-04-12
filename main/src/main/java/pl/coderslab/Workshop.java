9883f9bd1ea9650db67fb1e5f122af314a6581cb
michlach <pocztako@poczta.onet.pl>
2024-04-12 11:37:52 +0200

    test2

--- main/src/main/java/pl/coderslab/Workshop.java
+++ main/src/main/java/pl/coderslab/Workshop.java
@@ -1,7 +1,8 @@
 package pl.coderslab;
 
 public class Workshop {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
     }
 }
