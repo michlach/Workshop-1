17358e1f68f5bdd547bbf2d8b728fdd5792bc919
michlach <pocztako@poczta.onet.pl>
2024-04-12 16:36:21 +0200

    ver z zaczytywanym plikiem

--- main/src/main/java/pl/coderslab/TaskManager.java
+++ main/src/main/java/pl/coderslab/TaskManager.java
@@ -11,11 +11,10 @@ import java.util.Scanner;
 public class TaskManager {
     static String[][] tasks;
 
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        loadDataToTab("tasks.csv");
 
-    {
-        String option= optionsList();
-
+        String option = optionsList();
         switch (option) {
             case "add":
                 addTask();
@@ -33,56 +32,46 @@ public class TaskManager {
             default:
                 System.out.println("Please select a correct option.");
         }
-
     }
 
     public static String optionsList() {
         System.out.println(ConsoleColors.BLUE + "Please select an option: ");
-        System.out.println(ConsoleColors.RESET +"add");
+        System.out.println(ConsoleColors.RESET + "add");
         System.out.println("remove");
         System.out.println("list");
         System.out.println("exit");
-
         Scanner scanner = new Scanner(System.in);
         String option = scanner.nextLine();
-        scanner.close();
         return option;
     }
 
     public static void addTask() {
-        Scanner scanner= new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please add task description: ");
         String description = scanner.nextLine();
-
         System.out.println("Please add task due date: ");
-        String dueDate=scanner.nextLine();
+        String dueDate = scanner.nextLine();
         System.out.println("Is your task is important: true/false");
         String isImportant = scanner.nextLine();
-
         if (tasks == null) {
             tasks = new String[1][3];
         } else {
             tasks = Arrays.copyOf(tasks, tasks.length + 1);
         }
-        tasks[tasks.length - 1] = new String[3];
-        tasks[tasks.length - 1][0] = description;
-        tasks[tasks.length - 1][1] = dueDate;
-        tasks[tasks.length - 1][2] = String.valueOf(isImportant);
-
+        tasks[tasks.length - 1] = new String[]{description, dueDate, isImportant};
     }
 
     public static void removeTask() {
     }
 
     public static void listTasks() {
-        if(tasks == null || tasks.length ==0) {
+        if (tasks == null || tasks.length == 0) {
             System.out.println("No tasks is available");
         } else {
             System.out.println("Tasks: ");
-            for (int i = 0; i< tasks.length; i++){
-                System.out.println((i+1) + ". Description: " + tasks[i][0] +
-                        ", " + "Due Date: " + tasks[i][1] + ", Important: " +
+            for (int i = 0; i < tasks.length; i++) {
+                System.out.println((i + 1) + ". Description: " + tasks[i][0] +
+                        ", Due Date: " + tasks[i][1] + ", Important: " +
                         tasks[i][2]);
             }
         }
@@ -91,32 +80,23 @@ public class TaskManager {
     public static void saveTasksToFile(String fileName) {
     }
 
-
-
-
-    public static String[][] loadDataToTab(String fileName) {
+    public static void loadDataToTab(String fileName) {
         Path dir = Paths.get(fileName);
         if (!Files.exists(dir)) {
             System.out.println("File not exist.");
-            System.exit(0);
+            return;
         }
-
-        String[][] tab = null;
         try {
-            List<String> strings = Files.readAllLines(dir);
-            tab = new String[strings.size()][strings.get(0).split(",").length];
-
-            for (int i = 0; i < strings.size(); i++) {
-                String[] split = strings.get(i).split(",");
-                for (int j = 0; j < split.length; j++) {
-                    tab[i][j] = split[j];
+            List<String> lines = Files.readAllLines(dir);
+            tasks = new String[lines.size()][3];
+            for (int i = 0; i < lines.size(); i++) {
+                String[] split = lines.get(i).split(",");
+                for (int j = 0; j < 3; j++) {
+                    tasks[i][j] = split[j];
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return tab;
     }
-
-
 }
